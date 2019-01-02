8dcc91cf9f30ae21676859e126e0791a0fa31848
andrew-madsen <andrewrobertmadsen@gmail.com>
2019-01-01 20:50:25 -0700

    Setup pipenv and initial files.

---
+++ test_simple_math.py
@@ -0,0 +1,11 @@
+import simple_math
+
+def test_add_1():
+    assert simple_math.add(5, 4) == 9
+
+def test_subtract_1():
+    assert simple_math.subtract(5, 4) == 1
+
+def test_subtract_2():
+    assert simple_math.subtract(4, 5) == -1
+
